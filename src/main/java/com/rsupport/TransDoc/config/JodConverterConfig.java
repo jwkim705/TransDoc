bd86beb5fa41c23eb1f00e0afacdd48f1c41dc0a
jaewon <jwkim@rsupport.com>
2025-04-03 20:38:37 +0900

    jod converter office manger locat-> remote에 따른 remote 추가

--- src/main/java/com/rsupport/TransDoc/config/JodConverterConfig.java
+++ src/main/java/com/rsupport/TransDoc/config/JodConverterConfig.java
@@ -5,11 +5,13 @@ import org.jodconverter.remote.RemoteConverter;
 import org.jodconverter.remote.office.RemoteOfficeManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JodConverterConfig {
 
     @Bean(initMethod = "start", destroyMethod = "stop")
+    @Primary
     public RemoteOfficeManager officeManager() {
         return RemoteOfficeManager.builder()
                 .urlConnection("http://localhost:2002") // URL of the remote LibreOffice server
