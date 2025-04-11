7bbd033d8cb19c57bc265e6b98eb68c91b4e925a
jaewon <jwkim@rsupport.com>
2025-04-11 11:17:01 +0900

    1.설정 변경

--- src/main/java/com/rsupport/TransDoc/config/CustomDocumentFormatRegistry.java
+++ src/main/java/com/rsupport/TransDoc/config/CustomDocumentFormatRegistry.java
@@ -1,13 +1,23 @@
 package com.rsupport.TransDoc.config;
 
+import org.jodconverter.core.document.DefaultDocumentFormatRegistry;
 import org.jodconverter.core.document.DocumentFamily;
 import org.jodconverter.core.document.DocumentFormat;
+import org.jodconverter.core.document.DocumentFormatRegistry;
 import org.jodconverter.core.document.SimpleDocumentFormatRegistry;
 
 public class CustomDocumentFormatRegistry extends SimpleDocumentFormatRegistry {
 
     public CustomDocumentFormatRegistry() {
         super();
+
+        DocumentFormatRegistry defaultRegistry = DefaultDocumentFormatRegistry.getInstance();
+
+        for (DocumentFamily family : DocumentFamily.values()) {
+            for (DocumentFormat format : defaultRegistry.getOutputFormats(family)) {
+                addFormat(format);
+            }
+        }
 
         // HWP 등록
         DocumentFormat hwp = DocumentFormat.builder()
@@ -25,25 +35,7 @@ public class CustomDocumentFormatRegistry extends SimpleDocumentFormatRegistry {
                 .inputFamily(DocumentFamily.TEXT)
                 .build();
 
-        // PPT 형식 등록 (PowerPoint 97-2003)
-        DocumentFormat ppt = DocumentFormat.builder()
-            .name("Microsoft PowerPoint")
-            .extension("ppt")
-            .mediaType("application/vnd.ms-powerpoint")
-            .inputFamily(DocumentFamily.PRESENTATION)
-            .build();
-
-        // PPTX 형식 등록 (PowerPoint 2007+)
-        DocumentFormat pptx = DocumentFormat.builder()
-            .name("Microsoft PowerPoint Open XML")
-            .extension("pptx")
-            .mediaType("application/vnd.openxmlformats-officedocument.presentationml.presentation")
-            .inputFamily(DocumentFamily.PRESENTATION)
-            .build();
-
         addFormat(hwp);
         addFormat(hwpx);
-        addFormat(ppt);
-        addFormat(pptx);
     }
 }
