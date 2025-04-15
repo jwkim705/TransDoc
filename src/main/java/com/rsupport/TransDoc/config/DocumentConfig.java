b0a0c215b179f561687b3cd5eb6c3c56582f2640
jaewon <jwkim@rsupport.com>
2025-04-15 17:58:46 +0900

    1.엑셀 설정 변경

--- src/main/java/com/rsupport/TransDoc/config/DocumentConfig.java
+++ src/main/java/com/rsupport/TransDoc/config/DocumentConfig.java
@@ -33,10 +33,6 @@ public class DocumentConfig {
         Map<String, Object> storeProperties = new HashMap<>();
         storeProperties.put("FilterName", "calc_pdf_Export"); // Calc PDF 내보내기 필터 지정
         storeProperties.put("FilterData", Map.of(
-            "Selection", "all",
-            "FitToPages", true,
-            "ScaleToPagesX", 1,
-            "ScaleToPagesY", 1,
             "ReduceImageResolution", true,
             "MaxImageResolution", 300
         ));
